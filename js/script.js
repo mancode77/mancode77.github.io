e11febb4da2126dd3f521c4e330f9e8132e9e2ff
aji <you@example.com>
2021-06-15 19:58:16 +0700

    membuat portfolio

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+
+let progress = document.getElementById('progerssbar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function(){
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  progress.style.height = `${progressHeight}%`;
+}
